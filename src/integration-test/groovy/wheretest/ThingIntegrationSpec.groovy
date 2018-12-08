edbad4322eae5cb2dabf13e8e1108e49f86c5109
mburns <mike.burns@afoundria.com>
2018-12-07 23:36:41 -0600

    fix bug in detachedCriteria test.
    criteria.build{} returns a new criteria, but the reference was to the
    older one, without the property comparison.

--- src/integration-test/groovy/wheretest/ThingIntegrationSpec.groovy
+++ src/integration-test/groovy/wheretest/ThingIntegrationSpec.groovy
@@ -51,13 +51,12 @@ class ThingIntegrationSpec extends Specification {
         Thing.withCriteria { eq "name", "thing 1" }.size() == 1
     }
 
-    void "test detachedCriteria -- fails, and the sql doesn't filter by name."() {
+    void "test detachedCriteria"() {
         given: "two things"
         setupData()
 
         and: "A detachedCriteria"
-        def criteria = new DetachedCriteria(Thing)
-        criteria.build {
+        def criteria = new DetachedCriteria(Thing).build {
             eq 'name', 'thing 1'
         }
         println "Running withDetachedCriteria"
@@ -71,30 +70,13 @@ class ThingIntegrationSpec extends Specification {
 
         given: "two things"
         setupData()
-        def queryClosure = { -> Thing.where { name == "thing 1" } }
+        def queryClosure = { -> Thing.where { name == "thing 1" }.list().size() }
 
         expect:
-        queryClosure.call().list().size() == 1  // works
+        queryClosure.call() == 1  // works
         Thing.where { name == "thing 1" }.list().size() == 1   // fails
     }
 
-    // these two tests just failed, but when wrapped in the
-    // spock 'where' block (or maybe because it is a closure)
-    @Unroll
-    void "Test two bad ones #name that failed above"() {
-        given: "two things"
-        setupData()
-
-        expect: "my finder returns 1 thing"
-        query.call() == expected
-
-        // the previous two test failed... why do they work using this 'where/unroll' syntax?
-        where:
-        name                | expected | query
-        "Detached Criteria" | 1        | { -> new DetachedCriteria(Thing).build { eq 'name', 'thing 1' }.list().size() }
-        "Where"             | 1        | { -> Thing.where { name == "thing 1" }.list().size() }
-
-    }
 
     void "test where with criteria syntax"() {
 
--- src/test/groovy/wheretest/ThingSpec.groovy
+++ src/test/groovy/wheretest/ThingSpec.groovy
@@ -1,5 +1,6 @@
 package wheretest
 
+
 import grails.gorm.DetachedCriteria
 import grails.testing.gorm.DomainUnitTest
 import spock.lang.Specification
@@ -38,16 +39,14 @@ class ThingSpec extends Specification implements  DomainUnitTest<Thing> {
         Thing.withCriteria { eq "name", "thing 1" }.size() == 1
     }
 
-    void "test detachedCriteria -- fails, and the sql doesn't filter by name."() {
+    void "test detachedCriteria"() {
         given: "two things"
         setupData()
 
         and: "A detachedCriteria"
-        def criteria = new DetachedCriteria(Thing)
-        criteria.build {
+        def criteria = new DetachedCriteria(Thing).build {
             eq 'name', 'thing 1'
         }
-        println "Running withDetachedCriteria"
 
         expect:
         criteria.list().size() == 1
@@ -58,30 +57,13 @@ class ThingSpec extends Specification implements  DomainUnitTest<Thing> {
 
         given: "two things"
         setupData()
-        def queryClosure = { -> Thing.where { name == "thing 1" } }
+        def queryClosure = { -> Thing.where { name == "thing 1" }.list().size() }
 
         expect:
-        queryClosure.call().list().size() == 1  // works
+        queryClosure() == 1  // works
         Thing.where { name == "thing 1" }.list().size() == 1   // fails
     }
 
-    // these two tests just failed, but when wrapped in the
-    // spock 'where' block (or maybe because it is a closure)
-    @Unroll
-    void "Test two bad ones #name that failed above"() {
-        given: "two things"
-        setupData()
-
-        expect: "my finder returns 1 thing"
-        query.call() == expected
-
-        // the previous two test failed... why do they work using this 'where/unroll' syntax?
-        where:
-        name                | expected | query
-        "Detached Criteria" | 1        | { -> new DetachedCriteria(Thing).build { eq 'name', 'thing 1' }.list().size() }
-        "Where"             | 1        | { -> Thing.where { name == "thing 1" }.list().size() }
-
-    }
 
     void "test where with criteria syntax"() {
 
